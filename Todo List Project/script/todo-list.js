b2f2a733ee9f74095cda814b9580164b2baa7604
Entue250 <edouardoniyomugabo@gmail.com>
2024-01-11 16:20:59 +0200

    display the entered words on the web page

--- Todo List Project/script/todo-list.js
+++ Todo List Project/script/todo-list.js
@@ -1,4 +1,21 @@
-const todoList = [];
+const todoList = ['make dinner','wash dishes'];
+
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHtml = '';
+
+    for (let i=0; i<todoList.length;i++){
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`
+        todoListHtml += html;
+    }
+    console.log(todoListHtml);
+    
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHtml;
+}
+
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
@@ -8,4 +25,6 @@ function addTodo() {
     console.log(todoList);
 
     inputElement.value = '';
+
+    renderTodoList();
 }
